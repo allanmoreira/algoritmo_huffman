7852ac67d369bee17001b2d6ce22896abfdc1b46
Allan Moreira <allanfelipe.moreira@gmail.com>
2016-06-27 01:43:55 -0300

    Versão Final do Algoritmo

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,5 +1,7 @@
 import algoritmo.Huffman;
 
+import java.util.List;
+
 /**
  * Created by allanmoreira on 02/06/16.
  */
--- src/main/java/algoritmo/Huffman.java
+++ src/main/java/algoritmo/Huffman.java
@@ -30,18 +30,25 @@ public class Huffman {
         }
     }
 
+    /**
+     * O(2n caracteres da palavra) + O(n par caractere/frequência)
+     *
+     */
     public void codifica(String palavra) {
         this.palavra = palavra;
-        frequenciaCaractere(palavra);
-        geraRaizes();
-        unirArvores();
-        codificaCaractere();
-        codificaPalavra();
-        escreveArquivoBinario();
+        frequenciaCaractere(palavra); // O(n caracteres da palavra)
+        geraRaizes();  // O(n [par caractere/frequência]) + O(n) do insertionSort
+        unirArvores(); // O(n [par caractere/frequência]) + O(n) do insertionSort + adicionarRaizNaArvore constante
+        codificaCaractere(); // O(n) para percorrer toda a árvore
+        codificaPalavra(); // O(n caracteres da palavra) + O(n bits gerados pela codificação)
+        escreveArquivoBinario(); // O(n bits gerados pela árvore)
     }
 
     /**
-     * Verifica a frequência de cada caractere e insere o par caractere/frequencia em um HashMap chamado tabelaDeFrequencias
+     * Verifica a frequência de cada caractere e insere o par caractere/frequencia em
+     * um HashMap chamado tabelaDeFrequencias.
+     *
+     * Sua complexidade é N caracteres da palavra.
      */
     private void frequenciaCaractere(String palavra){
         tabelaDeFrequencias = new HashMap<Character, Integer>();
@@ -58,6 +65,8 @@ public class Huffman {
     /**
      * Pega cada par do HashMap @tabelaDeFrequencias e os insere em uma lista de raízes chamada @listaRaizes.
      * A cada nova raiz inserida na lista, utiliza o insertionSort para manter a lista ordenda em ordem decrescente.
+     *
+     * Sua complexidade é N par caractere/frequência + insertionSort
      */
     private void geraRaizes(){
         listaRaizes = new ArrayList<Nodo>();
@@ -70,6 +79,8 @@ public class Huffman {
     /**
      * Percorre a lista de raízes procurando as duas menores frequências e quando as encontra,
      * utiliza o método auxiliar adicionarRaizNaArvore que une a raiz à árvore final.
+     * 
+     * Sua complexidade é N caracteres da palavra que não se repetem + insertionSort
      */
     private void unirArvores(){
         while(listaRaizes.size() > 1){
@@ -84,6 +95,8 @@ public class Huffman {
 
     /**
      * Une as duas árvores escolhidas como as menores, adicionando-as na árvore final.
+     *
+     * Sua complexidade é constante.
      */
     private Nodo adicionarRaizNaArvore(Nodo maior, Nodo menor){
         Nodo raiz = new Nodo(' ', maior.frequencia + menor.frequencia);
@@ -97,36 +110,13 @@ public class Huffman {
         }
         return raiz;
     }
-
-    /**
-     * Chama o método auxiliar codificaCaractere, que gera o código binário de cada caractere,
-     * gera um array de inteiros que representa o código binário da palavra e
-     * gera um arquivo com esse código binário com o método escreveArquivoBinario.
-     */
-    private void codificaPalavra(){
-
-        // Gera um array com todos os caracteres da palavra inicial
-        char[] chars = palavra.toCharArray();
-        StringBuilder sb = new StringBuilder();
-
-        // Para cada caractere, adiciona na StringBuilder o código em binário referente àquela caractere
-        for (char c : chars)
-            sb.append(tabelaDeCodigosDosCaracteres.get(c));
-
-        // Transforma a StringBuilder de binparios em um array de inteiros
-        chars = sb.toString().toCharArray();
-        stringDeBits = new int[sb.length()];
-        for (int i = 0; i < chars.length; i++) {
-            stringDeBits[i] = Integer.parseInt(String.valueOf(chars[i]));
-        }
-
-        System.out.println("Codificação da palavra:   " + sb.toString());
-    }
-
+    
     /**
      * Gera o código binário de cada caractere. Percorre a árvore em  pŕe ordem, e cada vez
      * que percorre o próximo nodo, adiciona 0 se for pra esquerda e 1 à direita.
      * Quando chega na folha, remove o último bit da String a cada retorno da recursão para poder prosseguir.
+     *
+     * Sua complexidade é a complexidade para percorrer toda a árvore, ou seja, N.
      */
     private void codificaCaractere(){
         tabelaDeCodigosDosCaracteres = new HashMap<Character, String>();
@@ -151,127 +141,38 @@ public class Huffman {
         }
 
     }
-
+    
     /**
-     * Chama o método auxiliar que lê o arquivo em binário, gera a String com codificação, e chama o método auxiliar
+     * Gera um vetor de inteiros que representa o codificação binária da palavra.
+     *
+     * Sua complexidade é N caracteres da palavra + N bits gerados pela codificação
      */
-    public void decodifica(){
-        byte[] byteArray = leArquivoBinario();
+    private void codificaPalavra(){
+
+        // Gera um Array com todos os caracteres da palavra inicial
+        char[] chars = palavra.toCharArray();
         StringBuilder sb = new StringBuilder();
-        stringDeBits = new int[byteArray.length];
-        for (int i = 0; i < byteArray.length; i++) {
-            stringDeBits[i] = byteArray[i];
-            sb.append(stringDeBits[i]);
+
+        // Para cada caractere, adiciona na StringBuilder o código em binário referente àquele caractere
+        for (char c : chars)
+            sb.append(tabelaDeCodigosDosCaracteres.get(c));
+
+        // Transforma a StringBuilder de binários em um Array de inteiros
+        chars = sb.toString().toCharArray();
+        stringDeBits = new int[sb.length()];
+        for (int i = 0; i < chars.length; i++) {
+            stringDeBits[i] = Integer.parseInt(String.valueOf(chars[i]));
         }
 
-        System.out.println("Código do arquivo:        " + sb.toString());
-        decodificaCodigoBinario();
-    }
-
-    private void decodificaCodigoBinario() {
-        StringBuilder sbAux = new StringBuilder();
-        StringBuilder palavraDecodificada = new StringBuilder();
-
-        int contAux;
-        boolean encontrou;
-        int cont = 0;
-
-        while (cont < stringDeBits.length) {
-            encontrou = false;
-            contAux = cont;
-            while (!encontrou) {
-                sbAux.append(stringDeBits[contAux]);
-
-                if (tabelaDeCaracteresDosCodigos.containsKey(sbAux.toString())) {
-//                    System.out.println(s);
-                    palavraDecodificada.append(String.valueOf(tabelaDeCaracteresDosCodigos.get(sbAux.toString())));
-                    sbAux.setLength(0);
-                    cont = contAux;
-                    encontrou = true;
-                }
-                else {
-                    contAux++;
-                }
-            }
-            cont++;
-        }
-
-        System.out.println("Decodificação do arquivo: "+palavraDecodificada.toString());
+        System.out.println("Codificação da palavra:   " + sb.toString());
     }
 
     /**
-     * InsertionSort percorrendo de forma decrescente
+     * Transforma o vetor de inteiros que representam a codificação da palavra em um vetor de bytes e
+     * gera um arquivo com essa codificação. 
+     *
+     * Sua complexidade é N bits gerados pela árvore.
      */
-    private List<Nodo> insertionSortDesc(List<Nodo> lista) {
-        List<Nodo> listaOrdenada = lista;
-        int i;
-        Nodo nodo;
-
-        for (int j = listaOrdenada.size()-2; j >= 0; j--) {
-            nodo = listaOrdenada.get(j);
-            i = j+1;
-            while(i<listaOrdenada.size() && listaOrdenada.get(i).frequencia > nodo.frequencia){
-                listaOrdenada.set(i-1,listaOrdenada.get(i));
-                i = i+1;
-            }
-            listaOrdenada.set(i-1, nodo);
-        }
-        return listaOrdenada;
-    }
-
-    /**
-     * Percorre a árvore em largura, montando os dados do graphviz nível a nível
-     */
-    public void geraGraphviz(){
-        StringBuilder sb = new StringBuilder();
-        Queue<Nodo> fila = new LinkedList<Nodo>();
-        fila.add(raiz);
-        System.out.println("\nGraphviz: \n\n");
-        sb.append("digraph G {\n");
-        String nodo;
-        String aux;
-
-
-        while(!fila.isEmpty()){
-            Nodo n = fila.remove();
-            nodo = "\""+n.frequencia+"\"";
-
-            if(n.esquerdo != null) {
-                if(n.esquerdo.caractere != ' ')
-                    aux = "\""+n.esquerdo.caractere +","+n.esquerdo.frequencia+"\"\n";
-                else
-                    aux = "\""+n.esquerdo.frequencia+"\"\n";
-
-                sb.append("\t").append(nodo).append("->").append(aux);
-
-                fila.add(n.esquerdo);
-            }
-            if(n.direito != null) {
-                if(n.direito.caractere != ' ')
-                    aux = "\""+n.direito.caractere +","+n.direito.frequencia+"\"\n";
-                else
-                    aux = "\""+n.direito.frequencia+"\"\n";
-
-                sb.append("\t"+nodo+"->"+aux);
-
-                fila.add(n.direito);
-            }
-        }
-        sb.delete(sb.length()-2, sb.length());
-        sb.append("\n}");
-
-        try {
-            FileWriter fileWriter = new FileWriter(caminhoArquivoGerarGraphviz);
-            fileWriter.write(sb.toString());
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
     private void escreveArquivoBinario () {
         String caminhoArquivo = caminhoArquivoBinario;
 
@@ -292,6 +193,37 @@ public class Huffman {
         }
     }
 
+    /**
+     * InsertionSort percorrendo de forma decrescente. Como a lista sempre está ordenada, sua complexidade é O(n)
+     */
+    private void insertionSortDesc(List<Nodo> lista) {
+        int i;
+        Nodo nodo;
+
+        for (int j = lista.size()-2; j >= 0; j--) {
+            nodo = lista.get(j);
+            i = j+1;
+            while(i< lista.size() && lista.get(i).frequencia > nodo.frequencia){
+                lista.set(i-1, lista.get(i));
+                i = i+1;
+            }
+            lista.set(i-1, nodo);
+        }
+    }
+
+    // ----------------------------------------------------------------------------------
+
+    public void decodifica(){
+        byte[] byteArray = leArquivoBinario(); // constante
+        converteBitsParaInt(byteArray); // O(n bits do arquivo)
+        decodificaCodigoBinario(); // O(n bits do arquivo)
+    }
+
+    /**
+     * Lê o arquivo binário gerado e retorna um vetor com os bits do arquivo.
+     * 
+     * Sua complexidade é constante;
+     */
     private byte[] leArquivoBinario () {
         String caminhoArquivo = caminhoArquivoBinario;
         FileInputStream fileInputStream = null;
@@ -313,4 +245,111 @@ public class Huffman {
 
         return bytes;
     }
+
+    /**
+     * Converte o vetor de bytes em um vetor de inteiros
+     *
+     * Sua complexidade é a quantidade N de bits no vetor.
+     */
+    private void converteBitsParaInt(byte[] byteArray){
+        StringBuilder sb = new StringBuilder();
+        stringDeBits = new int[byteArray.length];
+        for (int i = 0; i < byteArray.length; i++) {
+            stringDeBits[i] = byteArray[i];
+            sb.append(stringDeBits[i]);
+        }
+        System.out.println("Código do arquivo:        " + sb.toString());
+    }
+
+    /**
+     * Lê um vetor de inteiros que representam os bits do arquivo um a um, procurando se existe um valor
+     * igual na tabela tabelaDeCaracteresDosCodigos. Caso não exista, concatena o próximo bit até encontrar 
+     * a codificação do caractere compatível. Encontrando, reinicia a ler o vetor da próxima posição
+     * não concatenada como valor encontrado.
+     *
+     * Sua complexidade é a quantidade de bits que representam a palavra.
+     */
+    private void decodificaCodigoBinario() {
+        StringBuilder sbAux = new StringBuilder();
+        StringBuilder palavraDecodificada = new StringBuilder();
+
+        int cont = 0;
+        int contAux;
+        boolean encontrou;
+
+        while (cont < stringDeBits.length) {
+            encontrou = false;
+            contAux = cont;
+            while (!encontrou) {
+                sbAux.append(stringDeBits[contAux]);
+
+                if (tabelaDeCaracteresDosCodigos.containsKey(sbAux.toString())) {
+                    palavraDecodificada.append(String.valueOf(tabelaDeCaracteresDosCodigos.get(sbAux.toString())));
+                    sbAux.setLength(0);
+                    cont = contAux;
+                    encontrou = true;
+                }
+                else {
+                    contAux++;
+                }
+            }
+            cont++;
+        }
+
+        System.out.println("Decodificação do arquivo: "+palavraDecodificada.toString());
+    }
+
+    /**
+     * Percorre a árvore em largura, montando os dados do graphviz nível a nível.
+     *
+     * Complexidade O(n)
+     */
+    public void geraGraphviz(){
+        StringBuilder sb = new StringBuilder();
+        Queue<Nodo> fila = new LinkedList<Nodo>();
+        fila.add(raiz);
+        System.out.println("\nGraphviz:\n");
+        sb.append("digraph G {\n");
+        String nAtual;
+        String aux;
+
+        while(!fila.isEmpty()){
+            Nodo n = fila.remove();
+            nAtual = "\""+n.frequencia+"\"";
+
+            if(n.esquerdo != null) {
+                if(n.esquerdo.caractere != ' ')
+                    aux = "\""+n.esquerdo.caractere +","+n.esquerdo.frequencia+"\"\n";
+                else
+                    aux = "\""+n.esquerdo.frequencia+"\"\n";
+
+                sb.append("\t").append(nAtual).append("->").append(aux);
+
+                fila.add(n.esquerdo);
+            }
+            if(n.direito != null) {
+                if(n.direito.caractere != ' ')
+                    aux = "\""+n.direito.caractere +","+n.direito.frequencia+"\"\n";
+                else
+                    aux = "\""+n.direito.frequencia+"\"\n";
+
+                sb.append("\t"+nAtual+"->"+aux);
+
+                fila.add(n.direito);
+            }
+        }
+        sb.delete(sb.length()-2, sb.length());
+        sb.append("\n}");
+
+        try {
+            FileWriter fileWriter = new FileWriter(caminhoArquivoGerarGraphviz);
+            fileWriter.write(sb.toString());
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(sb.toString());
+    }
+
 }
